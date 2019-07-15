0e77fef68dc38ab82867da6271a7ae086a34395f
houssam <houssam_houriem@hotmail.fr>
2019-07-15 20:23:00 +0100

    install react-router-dom and add it to the root component App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import { browserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Houssam</h1>
-      </div>
+      <browserRouter>
+        <div className="App">
+          <h1>Houssam</h1>
+        </div>
+      </browserRouter>
     );
   }
 
